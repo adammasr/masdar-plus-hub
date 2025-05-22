98299b520b737fa19b5471a7f794722d6e002dc8
adammasr <ahmedmagdypress@gmail.com>
2025-05-22 14:26:36 +0300

    Update aspect-ratio.tsx

--- src/components/ui/aspect-ratio.tsx
+++ src/components/ui/aspect-ratio.tsx
@@ -1,8 +1,15 @@
+import * as React from "react";
 import * as AspectRatioPrimitive from "@radix-ui/react-aspect-ratio";
 
 /**
  * AspectRatio component for maintaining a fixed aspect ratio for its children.
+ * يدعم تمرير جميع props الخاصة بـ Radix بما فيها ref
  */
-const AspectRatio = AspectRatioPrimitive.Root;
+const AspectRatio = React.forwardRef<
+  React.ElementRef<typeof AspectRatioPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof AspectRatioPrimitive.Root>
+>((props, ref) => <AspectRatioPrimitive.Root ref={ref} {...props} />);
+
+AspectRatio.displayName = "AspectRatio";
 
 export { AspectRatio };
