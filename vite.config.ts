bc0c06df83e40704d6e486e86393e5e553a34912
adammasr <ahmedmagdypress@gmail.com>
2025-05-22 12:53:00 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,12 +12,12 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      // "public": path.resolve(__dirname, "./public"),
     },
   },
 }));
