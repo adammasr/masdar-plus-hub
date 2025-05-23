41d4cb0c9e8e1eaa50735e714125334b8c892ef8
adammasr <ahmedmagdypress@gmail.com>
2025-05-23 16:29:20 +0300

    Update Layout.tsx

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,42 +1,36 @@
-
 import { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
 import AdminSidebar from "./AdminSidebar";
+import SidebarAd from "../SidebarAd"; // استيراد الإعلان الجانبي
 
-// Define the type for Layout props
 export interface LayoutProps {
   isAdmin?: boolean;
 }
 
 const Layout = ({ isAdmin = false }: LayoutProps) => {
-  // Scroll to top on route change
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
   return (
-  <div className="flex min-h-screen flex-col">
-    <Header />
-    {isAdmin ? (
-      <div className="flex flex-1 pt-16">
-        <AdminSidebar />
-        <main className="flex-1 p-4 ml-0 md:ml-64">
-          <Outlet />
-        </main>
-      </div>
-    ) : (
-      <main className="flex-1 pt-16">
-        <Outlet />
-      </main>
-    )}
-    <Footer />
-  </div>
-);
-        <main className="flex-1 pt-16">
-          <Outlet />
-        </main>
+    <div className="flex min-h-screen flex-col">
+      <Header />
+      {isAdmin ? (
+        <div className="flex flex-1 pt-16">
+          <AdminSidebar />
+          <main className="flex-1 p-4 ml-0 md:ml-64">
+            <Outlet />
+          </main>
+        </div>
+      ) : (
+        <div className="flex-1 pt-16 flex flex-row-reverse">
+          <main className="flex-1">
+            <Outlet />
+          </main>
+          <SidebarAd />
+        </div>
       )}
       <Footer />
     </div>
